1e3fc5743f469253656831476a5926fad6c026d6
FernandoMartinezHernandez <fernandomartinez737@aragon.unam.mx>
2022-05-19 17:07:13 -0500

    Integracion de las paginas individuales 5/9

--- proyecto_final/src/routes/users.js
+++ proyecto_final/src/routes/users.js
@@ -7,27 +7,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/jerico', function(req, res, next) {
-  res.render('equipo/PaginaDJHM/jerico');
+  res.render('equipo/PaginaDJHM/jerico',{style:'styleJerico.css', img:'DJHM.jpg'});
+});
+
+router.get('/alan', function(req, res, next) {
+  res.render('equipo/PaginaAGM/Alan',{style:'styleAlan.css', img:'AGM.jpg'});
 });
 
 router.get('/dana', function(req, res, next) {
-  res.render('equipo/PaginaDIFH/DanaItzelFonsecaHernandez');
+  res.render('equipo/PaginaDIFH/DanaItzelFonsecaHernandez',{style:'styleDana.css', img:'difh.jpg'});
 });
 
 router.get('/fernando', function(req, res, next) {
-  res.render('equipo/PaginaFMH/fernan');
+  res.render('equipo/PaginaFMH/fernan',{style:'styleFernan.css', img:'FMH.jpg'});
 });
 
 router.get('/hassel', function(req, res, next) {
-  res.render('equipo/PaginaHJP/hassel');
+  res.render('equipo/PaginaHJP/hassel',{style:'styleHassel.css', img:'HJP.jpg'});
 });
 
 router.get('/javier', function(req, res, next) {
-  res.render('equipo/PaginaACR/index');
+  res.render('equipo/PaginaJACR/javier',{style:'styleJavier.css', img:'profile.jpeg', img2:'banner.png'});
 });
 
 router.get('/jose', function(req, res, next) {
-  res.render('equipo/PaginaJMRR/index');
+  res.render('equipo/PaginaJMRR/jose',{style:'styleJose.css'});
 });
 
 router.get('/luis', function(req, res, next) {
