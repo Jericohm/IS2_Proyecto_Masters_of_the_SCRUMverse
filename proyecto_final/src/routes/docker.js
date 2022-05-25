db19a1646628ef8a18145791016719cb2accec99
Luis <luisvillalpando316@aragon.unam.mx>
2022-05-25 18:38:10 -0500

    añadi pagina principal de docker

--- proyecto_final/src/routes/docker.js
+++ proyecto_final/src/routes/docker.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('dockerI',{style:'styleDocker.css',intro:'/docker/intro',imagenes:'/docker/imagenes'
+  ,directivas:'/docker/directivas',ciclodevida:'/docker/ciclodevida', contenedores:'/docker/contenedores',archivo:'/docker/archivo'});
 });
 
 router.get('/intro', function(req, res, next) {
