e0f0322b1d52f471dbffc316e309f44d1468472c
FernandoMartinezHernandez <fernandomartinez737@aragon.unam.mx>
2022-05-26 16:54:10 -0500

    botons de regreso docker

--- proyecto_final/src/routes/docker.js
+++ proyecto_final/src/routes/docker.js
@@ -8,27 +8,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/intro', function(req, res, next) {
-  res.render('docker/PaginaIntro/Intro',{style:'styleIntro.css'});
+  res.render('docker/PaginaIntro/Intro',{regr:'/users',regr:'/docker'});
 });
 
 router.get('/imagenes', function(req, res, next) {
-  res.render('docker/PaginaImagenes/Imagenes',{img:'imagenes.png'});
+  res.render('docker/PaginaImagenes/Imagenes',{img:'imagenes.png',regr:'/docker'});
 });
 
 router.get('/directivas', function(req, res, next) {
-  res.render('docker/PaginaDirectivas/Directivas',{img:'imagenes.png'});
+  res.render('docker/PaginaDirectivas/Directivas',{img:'imagenes.png',regr:'/docker'});
 });
 
 router.get('/ciclodevida', function(req, res, next) {
-  res.render('docker/PaginaCiclosDeVida/CiclosDeVida',{img:'contenedores.jpg'});
+  res.render('docker/PaginaCiclosDeVida/CiclosDeVida',{img:'contenedores.jpg',regr:'/docker'});
 });
 
 router.get('/contenedores', function(req, res, next) {
-  res.render('docker/PaginaContenedores/Contenedores',{img:'cont.jpg', img2:'cont2.jpg'});
+  res.render('docker/PaginaContenedores/Contenedores',{img:'cont.jpg', img2:'cont2.jpg',regr:'/docker'});
 });
 
 router.get('/archivo', function(req, res, next) {
-  res.render('docker/PaginaArchivo/Archivo',{img:'image1.png'});
+  res.render('docker/PaginaArchivo/Archivo',{img:'image1.png', regr:'/docker'});
 });
 
 module.exports = router;
