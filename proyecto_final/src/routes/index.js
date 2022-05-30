38790f60f4454efc60927b4d85f183c818fcbe4c
unknown <alangarciamen221@gmail.com>
2022-05-29 23:26:54 -0500

    añadi los botones al index

--- proyecto_final/src/routes/index.js
+++ proyecto_final/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{logo:'Logo.jpg'});
+  res.render('index',{logo:'Logo.jpg',regr:'/users',regr1:'/docker',regr2:'/datos'});
 });
 
 module.exports = router;
